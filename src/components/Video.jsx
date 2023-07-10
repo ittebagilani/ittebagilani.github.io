23572fd494ea66a5aec148c8ff911e45f66cb3da
Itteba Gilani <itteba1@gmail.com>
2023-07-10 18:48:52 -0400

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,7 @@
 const Video = () => {
     return (
       <video controls width="80%" className="m-auto pb-20">
-        <source src="/src/assets/vids/lvl1.mp4" type="video/mp4" />
+        <source src="./src/assets/vids/lvl1.mp4" type="video/mp4" />
         Sorry, your browser doesn't support videos.
       </video>
     );
