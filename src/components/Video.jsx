30d6b5b20ed26249b752d81c3a99e34bc9141c42
Itteba Gilani <itteba1@gmail.com>
2023-07-10 21:38:39 -0400

    update vid

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,8 +5,8 @@ const VIDEO_PATH = 'https://youtu.be/Ygnou642f2Q';
 function Video() {
    const playerRef = useRef(null);
    return (
-      <div>
-         <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} />
+      <div className='m-auto'>
+         <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} className="m-auto pb-10" width={1000} height={700} />
       </div>
    )
 };
