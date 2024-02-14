7fc3e420f112520da6ce7baded96425c7d70fa6b
Itteba Gilani <itteba1@gmail.com>
2024-02-14 09:04:25 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
+
+
+const tagManagerArgs = {
+    gtmId: 'GTM-NNNQ6PW4'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -12,7 +20,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </BrowserRouter>,
 )
 
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-DWHN2KCD93"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
 
+//   gtag('config', 'G-DWHN2KCD93');
+// </script>
 // ReactDOM.createRoot((
 //   <BrowserRouter>
 //     <App /> 
